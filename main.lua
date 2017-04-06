5ed8caa391e4a83ee587b827150f5a27d175af48
GIACOMO ESPOSITO <espomagic@live.com>
2017-04-06 04:36:02 +0200

    n

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ local sti = require 'sti'
 local bump = require 'bump'
 local sodapop = require "assets/player/sodapop"
 local player = require "assets/player/player"
-
+exit = {}
 
 -- Creazione mondo
 local world = bump.newWorld()
@@ -24,32 +24,36 @@ local function filterMovement(item, other)
 
 function love.load(arg)
 
-
-
-
-
-  --Carico mappa iniziale
+--Carico mappa iniziale
   map = sti("map/livello iniziale/prigione2.lua", {"bump"})
   map:bump_init(world)
-
 
 
 for k, object in pairs(map.objects) do
   if object.name == "spawn" then
     player.x = object.x
     player.y = object.y
+  elseif object.name == "stair" then
+      exit.x = object.x
+      exit.y = object.y
 
+  end
 end
-end
+
+
+
 world:add(player, player.x, player.y, player.w, player.h)
 player.load (world)
+
+
+
+if player.x == exit.x and player.y == exit.y then
+  function loadmap("map/mappa principale/mainmap.lua")
+
+
 end
 
-
-
-
-
-
+end
 
 
 
@@ -59,8 +63,11 @@ end
 
 function love.update(dt)
 
-player.update (dt)
-    end
+player.update (dt, int)
+
+
+
+end
 
 
 
